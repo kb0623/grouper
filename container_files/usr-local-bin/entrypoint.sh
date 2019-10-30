7999713f4510f7902a00a9485c4dce8aa011e098
Christopher Hubing <chubing@internet2.edu>
2019-10-30 16:10:53 -0400

    Update entrypoint.sh

--- container_files/usr-local-bin/entrypoint.sh
+++ container_files/usr-local-bin/entrypoint.sh
@@ -3,4 +3,7 @@
 . /usr/local/bin/library.sh
 prepConf
 
+echo Checking availability of subject sources
+while ! ./bin/gsh.sh -runarg getSources\(\) >/dev/null ; do echo waiting for grouper subject sources to start; sleep 1; done;
+
 exec "$@"
